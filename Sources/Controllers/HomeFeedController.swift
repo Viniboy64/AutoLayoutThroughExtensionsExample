c38c7cb6beccc4604bbfac7ad89b1f68d513a53d
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-24 18:09:30 -0300

    home-feed-controller-redesign

--- Sources/Controllers/HomeFeedController.swift
+++ Sources/Controllers/HomeFeedController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class HomeFeedController: UIViewController {
+    private let purpleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .purple
+        
+        return view
+    }()
+    
     private let redView: UIView = {
         let view = UIView()
         view.backgroundColor = .red
@@ -30,6 +37,20 @@ class HomeFeedController: UIViewController {
         return view
     }()
     
+    private let cyanView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        
+        return view
+    }()
+    
+    private let orangeView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .orange
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -42,36 +63,66 @@ class HomeFeedController: UIViewController {
     
     private func setupLayout() {
         // Add subviews to view.
-        view.addSubviews([redView, greenView, blueView])
+        view.addSubviews([purpleView, redView, greenView, blueView, cyanView, orangeView])
+        
+        // Constraints for purpleView.
+        purpleView.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            leading: view.safeAreaLayoutGuide.leadingAnchor,
+            bottom: nil,
+            trailing: nil,
+            padding: UIEdgeInsets(top: 12.0, left: 12.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 231.0, height: 231.0)
+        )
         
         // Constraints for redView.
         redView.anchor(
             top: view.safeAreaLayoutGuide.topAnchor,
-            leading: nil,
+            leading: purpleView.trailingAnchor,
             bottom: nil,
-            trailing: view.safeAreaLayoutGuide.trailingAnchor,
-            padding: UIEdgeInsets(top: 16.0, left: 0.0, bottom: 0.0, right: 16.0),
-            size: CGSize(width: 96.0, height: 96.0)
+            trailing: nil,
+            padding: UIEdgeInsets(top: 12.0, left: 12.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 109, height: 109)
         )
         
         // Constraints for greenView.
         greenView.anchor(
             top: redView.bottomAnchor,
-            leading: nil,
+            leading: purpleView.trailingAnchor,
             bottom: nil,
-            trailing: view.safeAreaLayoutGuide.trailingAnchor,
-            padding: UIEdgeInsets(top: 16.0, left: 0.0, bottom: 0.0, right: 16.0),
-            size: CGSize(width: 96.0, height: 96.0)
+            trailing: nil,
+            padding: UIEdgeInsets(top: 12.0, left: 12.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 109, height: 109)
         )
-        
+
         // Constraints for blueView.
         blueView.anchor(
-            top: greenView.bottomAnchor,
-            leading: nil,
+            top: purpleView.bottomAnchor,
+            leading: view.safeAreaLayoutGuide.leadingAnchor,
             bottom: nil,
-            trailing: view.safeAreaLayoutGuide.trailingAnchor,
-            padding: UIEdgeInsets(top: 16.0, left: 0.0, bottom: 0.0, right: 16.0),
-            size: CGSize(width: 96.0, height: 96.0)
+            trailing: nil,
+            padding: UIEdgeInsets(top: 12.0, left: 12.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 109.0, height: 109.0)
+        )
+
+        // Constraints for cyanView.
+        cyanView.anchor(
+            top: purpleView.bottomAnchor,
+            leading: blueView.trailingAnchor,
+            bottom: nil,
+            trailing: nil,
+            padding: UIEdgeInsets(top: 12.0, left: 12.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 109.0, height: 109.0)
+        )
+        
+        // Constraints for orangeView.
+        orangeView.anchor(
+            top: greenView.bottomAnchor,
+            leading: cyanView.trailingAnchor,
+            bottom: nil,
+            trailing: nil,
+            padding: UIEdgeInsets(top: 12.0, left: 12.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 109.0, height: 109.0)
         )
     }
 }
