2fb8f040d4e1e050f457a49d598d056856945977
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-24 15:42:35 -0300

    single-view-added-to-project

--- Sources/Controllers/HomeFeedController.swift
+++ Sources/Controllers/HomeFeedController.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class HomeFeedController: UIViewController {
+    private let redView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        
+        // This enables autolayout to view.
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "HOME"
         
-        view.backgroundColor = .red
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        // Add subviews to view.
+        view.addSubview(redView)
+        
+        // Constraints for redView.
+        NSLayoutConstraint.activate(
+            [
+                redView.topAnchor.constraint(equalTo: view.topAnchor),
+                redView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                redView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                redView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            ]
+        )
     }
 }
