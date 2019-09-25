beb94e045d0dccc6ca9e6496244734ed0e94b8b6
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-24 21:05:21 -0300

    home-feed-controller-changed

--- Sources/Controllers/HomeFeedController.swift
+++ Sources/Controllers/HomeFeedController.swift
@@ -9,47 +9,12 @@
 import UIKit
 
 class HomeFeedController: UIViewController {
-    private let purpleView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .purple
-        
-        return view
-    }()
-    
-    private let redView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .red
-        
-        return view
-    }()
-    
-    private let greenView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .green
-        
-        return view
-    }()
-    
-    private let blueView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .blue
-        
-        return view
-    }()
-    
-    private let cyanView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .cyan
-        
-        return view
-    }()
-    
-    private let orangeView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .orange
-        
-        return view
-    }()
+    private let purpleView = UIView(backgroundColor: .purple)
+    private let redView = UIView(backgroundColor: .red)
+    private let greenView = UIView(backgroundColor: .green)
+    private let blueView = UIView(backgroundColor: .blue)
+    private let cyanView = UIView(backgroundColor: .cyan)
+    private let orangeView = UIView(backgroundColor: .orange)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,14 +31,6 @@ class HomeFeedController: UIViewController {
         view.addSubviews([purpleView, redView, greenView, blueView, cyanView, orangeView])
         
         // Constraints for purpleView.
-//        purpleView.anchor(
-//            top: view.safeAreaLayoutGuide.topAnchor,
-//            leading: view.safeAreaLayoutGuide.leadingAnchor,
-//            bottom: nil,
-//            trailing: nil,
-//            padding: UIEdgeInsets(top: 12.0, left: 12.0, bottom: 0.0, right: 0.0),
-//            size: CGSize(width: 231.0, height: 231.0)
-//        )
         purpleView.anchor(
             top: view.safeAreaLayoutGuide.topAnchor,
             leading: view.safeAreaLayoutGuide.leadingAnchor,
